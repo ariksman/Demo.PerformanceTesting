e1b37fd0df8aa8fc73e08d43a9452d3ab9457256
Antti Riksman <antti.riksman@zure.com>
2021-11-07 18:20:12 +0200

    simple .NET 6 background task runner on intervals class

---
+++ Demo.BackgroundTimer/BackgroundTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Demo.BackgroundTimer
+{
+  public class BackgroundTask
+  {
+    private Task? _timerTask;
+    private readonly CancellationTokenSource _cts = new();
+
+    public void StartAsync(TimeSpan timeSpan, Func<Task> action)
+    {
+      async Task DoStart()
+      {
+        try
+        {
+          var timer = new PeriodicTimer(timeSpan);
+          while(await timer.WaitForNextTickAsync(_cts.Token))
+          {
+            await action();
+          }
+        }
+        catch (OperationCanceledException e){}
+      }
+
+      _timerTask = DoStart();
+    }
+
+    public async Task StopAsync()
+    {
+      _cts.Cancel();
+
+      await _timerTask;
+
+      _cts.Dispose();
+
+      Console.WriteLine("Background task stopped");
+    }
+  }
+}
---
+++ Demo.BackgroundTimer/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Demo.BackgroundTimer;
+
+class Program
+{
+  public static async Task Main(string[] args)
+  {
+    Console.WriteLine("Hello, World!");
+
+    var task = new BackgroundTask();
+    task.StartAsync(TimeSpan.FromSeconds(2), () => Task.Run(() => Console.WriteLine($"Tick, {DateTime.UtcNow:O}")));
+
+    Console.ReadKey();
+
+    await task.StopAsync();
+
+    Console.ReadKey();
+  }
+}
